e2badee2caf773545a47b5c6f3e1d0ed5e65b9ab
Michael Clark <michaelallenclark@gmail.com>
2012-11-28 11:14:04 -0500

    Updated for Step 4

--- index.php
+++ index.php
@@ -12,17 +12,75 @@ require_once 'FuelAPI-Platform.php';
         <meta name="description" content="Node-Backbone-FuelUX based App">
         <title>Subscriber Search - PHP Version</title>
         <link rel="stylesheet" href="/css/styles.css" />
-        <link rel="stylesheet" href="/vendor/fuelux/css/fuelux.min.css" />
-        <link rel="stylesheet" href="/vendor/fuelux/css/fuelux-responsive.min.css" />
-        <link rel="shortcut icon" href="/img/fuel-icon.gif">
-        <script src="//cdnjs.cloudflare.com/ajax/libs/require.js/2.0.6/require.min.js"></script>
-        <script src="/vendor/fuelux/all.min.js"></script>
-        <script src="/js/app-config.js"></script>
-        <script src="//use.edgefonts.net/ubuntu-condensed.js"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.1/jquery.min.js" type="text/javascript"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js" type="text/javascript"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.1.1/bootstrap.min.js" type="text/javascript"></script>		
-		<script src="//cdnjs.cloudflare.com/ajax/libs/datejs/1.0/date.min" type="text/javascript.js"></script>
+		
+		<link href="/vendor/fuelux/css/fuelux.css" rel="stylesheet" />
+		<link href="/vendor/fuelux/css/fuelux-responsive.css" rel="stylesheet">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js" type="text/javascript"></script>
+		<script src="http://raw.github.com/ExactTarget/fuelux/2.0.0/sample/datasource.js" type="text/javascript"></script>
+		<script src="/vendor/fuelux/loader.js" type="text/javascript"></script>
+
+		
+		<script  type="text/javascript">
+			
+			$(function () {
+				//if search
+				$('#subscriberSearch').on('searched', function (e, text) {					
+					var $url = "/soap-SubSearch.php?searchString=" + text;
+					console.log($url)								
+					$.ajax({url: $url, 				
+						
+						complete:function(result){
+							
+							var subs = JSON.parse(result.responseText).subscribers;
+							
+							$.each( subs, function( index, item ) {
+
+								item.ViewDetails = '<button type="button" class="btn btn-primary viewDetails" id="' + item.ID + '">View Details</button>';
+								console.log('item');
+								console.log(item);
+							});
+
+							var dataSource = new StaticDataSource({
+								columns: [{
+									property: 'EmailAddress',
+									label: 'Email Address',
+									sortable: true
+									}, {
+									property: 'SubscriberKey',
+									label: 'Subscriber Key',
+									sortable: true
+									},
+									{
+									property: 'ViewDetails',
+									label: '',
+									sortable: true
+									}, 
+									{
+									property: 'Status',
+									label: 'Status',
+									sortable: true
+								}
+								],					
+								data: subs,
+								delay: 250
+							});
+
+							
+							$('#subscriberGrid').datagrid({
+								dataSource: dataSource
+							});	
+							$( '#subscriberGrid' ).datagrid( 'renderData' );
+						}		
+					});		
+				});
+			});
+			
+			
+			$(document).ready(function() {				
+
+			});	
+		</script>
         
 <link rel="stylesheet" href="/css/index.css"/>
 
@@ -42,7 +100,7 @@ require_once 'FuelAPI-Platform.php';
 			<div id="subscriberDetails"></div>
 			</div>
 		</div>
-		<div id="subscriberGridContainer" class="span9"><table class="table table-bordered datagrid" id="subscriberGrid">
+		<div class="span9"><table id="subscriberGrid" class="table table-bordered datagrid" >
 			<thead>
 				<tr>
 					<th colspan="4">
@@ -56,33 +114,44 @@ require_once 'FuelAPI-Platform.php';
 						</div>
 					</th>
 				</tr>
-			<tr><th class="sortable" data-property="emailAddress">Email Address</th><th class="sortable" data-property="fullName">Full Name</th><th data-property="viewDetails"></th><th class="sortable" data-property="status">Status</th></tr></thead><tbody><tr><td colspan="4" style="text-align:center;padding:20px;"><div style="width:50%;margin:auto;" class="progress progress-striped active"><div style="width:100%;" class="bar"></div></div></td></tr></tbody>
-			
-			<tfoot>
-				<tr>
-					<th colspan="4">
-						<div style="display: none;" class="datagrid-footer-left">
-							<div class="grid-controls">
-								<span><span class="grid-start"></span> - <span class="grid-end"></span> of <span class="grid-count"></span></span>
-								<select class="grid-pagesize"><option>10</option><option>20</option><option>50</option><option>100</option></select>
-								<span>Per Page</span>
-							</div>
-						</div>
-						<div style="display:none;" class="datagrid-footer-right">
-							<div class="grid-pager">
-								<button class="btn grid-prevpage"><i class="icon-chevron-left"></i></button>
-								<span>Page</span>
-								<div class="input-append dropdown combobox">
-									<input type="text" class="span1"><button data-toggle="dropdown" class="btn"><i class="caret"></i></button>
-									<ul class="dropdown-menu"></ul>
+				<tfoot>
+					<tr>
+						<th>
+							<div class="datagrid-footer-left" style="display:none;">
+								<div class="grid-controls">
+									<span><span class="grid-start"></span> - <span class="grid-end"></span> of
+									<span class="grid-count"></span></span>
+									<select class="grid-pagesize">
+										<option>10</option>
+										<option>20</option>
+										<option>50</option>
+										<option>100</option>
+									</select>
+									<span>Per Page</span>
 								</div>
-								<span>of <span class="grid-pages"></span></span>
-								<button class="btn grid-nextpage"><i class="icon-chevron-right"></i></button>
 							</div>
-						</div>
-					</th>
-				</tr>
-			</tfoot>
+							<div class="datagrid-footer-right" style="display:none;">
+								<div class="grid-pager">
+									<button class="btn grid-prevpage">
+										<i class="icon-chevron-left"></i>
+									</button>
+									<span>Page</span>
+									<div class="input-append dropdown combobox">
+										<input class="span1" type="text">
+										<button class="btn" data-toggle="dropdown">
+											<i class="caret"></i>
+										</button>
+										<ul class="dropdown-menu"></ul>
+									</div>
+									<span>of <span class="grid-pages"></span></span>
+									<button class="btn grid-nextpage">
+										<i class="icon-chevron-right"></i>
+									</button>
+								</div>
+							</div>
+						</th>
+					</tr>
+				</tfoot>
 			</table>
 		</div>
 		</div>
---
+++ soap-SubSearch.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once("exacttarget_soap_client.php");
+require_once("config.php");
+require_once("FuelAPI-Platform.php");
+
+//Check to see if we have started the session already.
+if(!isset($_SESSION)) {session_start();}
+
+$token = $_SESSION['internalOauthToken'];
+
+$searchString = $_GET['searchString'];	
+
+$soapURL = getSoapURLFromPlatform();
+
+
+try{
+
+	$client = new ExactTargetSoapClient('etframework.wsdl', array('trace'=>1));
+	
+	$client->__setLocation($soapURL);
+	
+
+	/* Set username and password here when passing an AuthToken, username and password are required but should be set to "*" */
+	$client->username = '*';
+	$client->password = '*';
+	$client->authtoken = $token;
+
+	$rr = new ExactTarget_RetrieveRequest();
+	$rr->ObjectType = 'Subscriber';
+
+	//Set the properties to return
+	$props = array( "ID", "EmailAddress", "SubscriberKey", "Status");
+	$rr->Properties = $props;
+
+	//Setup account filtering, to look for a given account MID
+	$filterPart = new ExactTarget_SimpleFilterPart();
+	$filterPart->Property = 'EmailAddress';
+	$filterPart->SimpleOperator = ExactTarget_SimpleOperators::like;
+	$filterPart->Value = $searchString;
+
+	$filterPart = new SoapVar($filterPart, SOAP_ENC_OBJECT,'SimpleFilterPart', "http://exacttarget.com/wsdl/partnerAPI");
+	$rr->Filter = $filterPart;
+
+	//Setup and execute request
+	$rrm = new ExactTarget_RetrieveRequestMsg();
+	$rrm->RetrieveRequest = $rr;
+
+	$results = $client->Retrieve($rrm);
+	
+	$output = array();
+	if (property_exists($results, "Results")){
+		if (is_array($results->Results)){		
+			foreach($results->Results as $sub) 
+			{		
+				$output = $results->Results;
+			} 		
+		} else {
+				$output = array();
+				$output[] = $results->Results;		
+		}
+	}
+	
+	$Subcontainer->subscribers = $output;
+	// Print the results as JSON so that they can be consumed using an ajax post
+	print_r (json_encode($Subcontainer));			
+} 
+catch (SoapFault $e) 
+{
+	print_r($e);
+	print_r(array());
+}	
+
+
+?>
